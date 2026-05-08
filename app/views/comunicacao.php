db7e5189826114d0c7fd7cb2a8fcb0e04d643125
Eduardo Martins <eduardoferreiramartins74@gmail.com>
2026-05-08 15:15:45 -0300

    fix: remove link para comunicacao_style.css inexistente

--- app/views/comunicacao.php
+++ app/views/comunicacao.php
@@ -31,7 +31,6 @@ if ($id_contato > 0) {
     <title>Chat - CEIControl</title>
     <link rel="stylesheet" href="/public/style.css">
     <link rel="stylesheet" href="/public/mobile.css">
-    <link rel="stylesheet" href="/public/comunicacao_style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="icon" type="image/png" href="/public/assets/ceicontrol.png">
     <style>
